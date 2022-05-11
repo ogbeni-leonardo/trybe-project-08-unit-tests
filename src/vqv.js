5786ee7873eb1b88e2887386910c007e0d6479e3
Leonardo C Araújo <ogbeni.leonardo@outlook.com>
2022-05-11 15:06:49 -0300

    Resolução do requisito 03

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,13 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (!name || !age) return undefined;
+  const message = `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return message;
+};
 
 module.exports = vqv;
